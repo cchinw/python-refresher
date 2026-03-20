9df37d2a20a9ff47ad5e6fa253ce97c02bd6d064
Chinwendu <chinwenduukoha@gmail.com>
2026-03-20 14:51:51 -0700

    Made code cleaner and readable

--- lesson5_functions.py
+++ lesson5_functions.py
@@ -4,15 +4,21 @@ children = ["Chidera", "Imole", "Layo", "Chance"]
 
 def greet_child(name, age):
   if name == "Chidera":
-    return "My beating heart" + " " + name + ", you are " + str(age) + " years old"
+    return f"My beating heart {name}, you are {age} years old"
   elif name == "Imole":
-    return "My bright light" + " " + name + ", you are " + str(age) + " years old"
+    return f"My bright light {name}, you are {age} years old"
   elif name == "Layo":
-    return "My joy bringer" + " " + name + ", you are " + str(age) + " years old"
+    return f"My joy bringer {name}, you are {age} years old"
   else:
-    return "My bundle of joy" + " " + name + ", you are " + str(age) + " years old"
-  
+    return f"My bundle of joy {name}, you are {age} years old"
+
 print(greet_child(children[0], 3))
 print(greet_child("Imole", 2))
 print(greet_child("Layo", 1))
 print(greet_child("Chance", 0))
+
+# # --- Exercise 2 ---
+
+# salaries = [115000,200000,280000,393000,550000]
+
+# def check_salary(salary):
